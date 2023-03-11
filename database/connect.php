290674f9a4ca9d1372f12c7bb6966ffb12422d8a
Miguel Cemiller <miguelcemiller@gmail.com>
2023-03-11 14:09:43 +0800

    added dashboard icons

--- database/connect.php
+++ database/connect.php
@@ -1,14 +1,11 @@
 <?php
 $user = "root";
 $pass = "";
-$server = "localhost";
+$server = "localhost:3307";
 $db = "majayjay_dbs";
-$conn = mysqli_connect($server,$user,$pass);
-$select_db = mysqli_select_db($conn,$db);
+$conn = mysqli_connect($server, $user, $pass);
+$select_db = mysqli_select_db($conn, $db);
 
 
 $timezone = "Asia/Singapore";
 date_default_timezone_set($timezone);
-
-
-?>
